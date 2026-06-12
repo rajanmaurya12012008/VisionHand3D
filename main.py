72ff338e45f432d6464eba229e0776f910a09b30
Rajan Maurya <rajan12maurya2008@gmail.com>
2026-06-12 21:09:49 +0530

    new modify

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2
 
 from hand import detect_hand
-from draw import draw_with_finger, clear_canvas
+from draw import draw_with_finger, clear_canvas, get_points
+from shape import detect_shape
 
 cap = cv2.VideoCapture(0)
 
@@ -9,28 +10,36 @@ while True:
 
     success, img = cap.read()
 
+    shape_name = ""
+
+    if not success:
+        print("Camera Error")
+        continue
+
     img = cv2.flip(img, 1)
 
     img, landmarks = detect_hand(img)
 
     if len(landmarks) != 0:
 
-        # Index finger tip
+        # Index Finger Tip
         x, y = landmarks[8]
 
         # Draw using finger
         draw_with_finger(img, x, y)
 
-        # Thumb tip
-        thumb_x, thumb_y = landmarks[4]
+        # Thumb Tip
+        thumb_x, thumb_y = landmarks[4] 
 
         # Distance between thumb & index
         distance = abs(x - thumb_x)
 
-        # Pinch gesture = clear screen
+        # Pinch gesture = Clear Canvas
         if distance < 40:
-
             clear_canvas()
+
+        # Detect Shape
+        shape_name = detect_shape(get_points())
 
     cv2.putText(
         img,
@@ -39,6 +48,16 @@ while True:
         cv2.FONT_HERSHEY_SIMPLEX,
         1,
         (255, 0, 0),
+        3
+    )
+
+    cv2.putText(
+        img,
+        shape_name,
+        (20, 100),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
         3
     )
 

5543e575172aafaeef6a4640731f65faeee24df4
Rajan Maurya <rajan12maurya2008@gmail.com>
2026-08-19 10:18:26 +0530

    new updates

--- draw.py
+++ draw.py
@@ -5,19 +5,32 @@ points = []
 
 def draw_with_finger(img, x, y):
 
-    points.append((x, y))
+    if len(points) == 0:
 
-    for i in range(1, len(points)):
+        points.append((x, y))
 
-        cv2.line(
-            img,
-            points[i - 1],
-            points[i],
-            (0, 255, 0),
-            5
-        )
+    elif abs(x - points[-1][0]) > 5 or abs(y - points[-1][1]) > 5:
+        
+        points.append((x, y))
+
+    if len(points) > 1:
+
+        for i in range(1, len(points)):
+
+            cv2.line(
+                img,
+                points[i -1],
+                points[i],
+                (0, 255, 0),
+                5
+            )
 
 
 def clear_canvas():
 
     points.clear()
+
+
+def get_points():
+
+    return points
